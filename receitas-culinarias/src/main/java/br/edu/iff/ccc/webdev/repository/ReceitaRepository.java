f0bb294115946cdbeacab2c437d5a6262c743210
Matheus Ridolphi <m.ridolphi@gsuite.iff.edu.br>
2025-09-29 11:15:49 -0300

    Atualização repositorio de receita, adicionado um query.

--- receitas-culinarias/src/main/java/br/edu/iff/ccc/webdev/repository/ReceitaRepository.java
+++ receitas-culinarias/src/main/java/br/edu/iff/ccc/webdev/repository/ReceitaRepository.java
@@ -1,6 +1,8 @@
 package br.edu.iff.ccc.webdev.repository;
-
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.edu.iff.ccc.webdev.entities.Receita;
 
@@ -8,4 +10,11 @@ public interface ReceitaRepository extends JpaRepository<Receita, Long> {
 
     // Para checar se um e-mail é dono da receita (ADMIN ou DONO)
     boolean existsByIdAndCriadoPorEmailIgnoreCase(Long id, String email);
+
+    @Query("select r from Receita r " +
+        "where (:q is null or lower(r.nome) like lower(concat('%', :q, '%'))) " +
+        "order by r.nome asc")
+    List<Receita> buscarPorNome(@Param("q") String q);
+
+    
 }
